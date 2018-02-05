0c3294359f186b20f3ef5c2f41f22b1114a2e4c4
Robert Vollmert <rob@vllmrt.net>
2018-02-05 21:52:32 +0100

    elm-format WebSocket/LowLevel.elm

--- elm/WebSocket/LowLevel.elm
+++ elm/WebSocket/LowLevel.elm
@@ -1,13 +1,19 @@
-module WebSocket.LowLevel exposing
-  ( WebSocket
-  , open, Settings
-  , send, close, closeWith
-  , bytesQueued
-  , BadOpen(..), BadClose(..), BadSend(..)
-  )
+module WebSocket.LowLevel
+    exposing
+        ( BadClose(..)
+        , BadOpen(..)
+        , BadSend(..)
+        , Settings
+        , WebSocket
+        , bytesQueued
+        , close
+        , closeWith
+        , open
+        , send
+        )
 
 {-| Low-level bindings to [the JavaScript API for web sockets][ws]. This is
-useful primarily for making effect modules like [WebSocket](WebSocket). So
+useful primarily for making effect modules like <WebSocket>. So
 if you happen to be the creator of Elixir’s Phoenix framework, and you want
 it to be super easy to use channels, this module will help you make a really
 nice subscription-based API. If you are someone else, you probably do not want
@@ -15,13 +21,19 @@ these things.
 
 [ws]: https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
 
+
 # WebSockets
+
 @docs WebSocket
 
+
 # Using WebSockets
+
 @docs open, Settings, send, close, closeWith, bytesQueued
 
+
 # Errors
+
 @docs BadOpen, BadClose, BadSend
 
 -}
@@ -37,15 +49,17 @@ it up once and keep using it. This means it is faster to send messages.
 There is a request/response pattern for all HTTP requests. Client asks for
 something, server gives some response. With websockets, you can drive messages
 from the server instead.
+
 -}
-type WebSocket = WebSocket
+type WebSocket
+    = WebSocket
 
 
 {-| Attempt to open a connection to a particular URL.
 -}
 open : String -> Settings -> Task BadOpen WebSocket
 open =
-  Native.WebSocket.open
+    Native.WebSocket.open
 
 
 {-| The settings describe how a `WebSocket` works as long as it is still open.
@@ -62,51 +76,52 @@ You will typically want to set up a channel before opening a WebSocket. That
 way the `onMessage` and `onClose` can communicate with the other parts of your
 program. **Ideally this is handled by the effect library you are using though.
 Most people should not be working with this stuff directly.**
+
 -}
 type alias Settings =
-  { onMessage : WebSocket -> String -> Task Never ()
-  , onClose : { code : Int, reason : String, wasClean : Bool } -> Task Never ()
-  }
+    { onMessage : WebSocket -> String -> Task Never ()
+    , onClose : { code : Int, reason : String, wasClean : Bool } -> Task Never ()
+    }
 
 
 {-| Opening the websocket went wrong because:
 
-  1. Maybe you are on an `https://` domain trying to use an `ws://` websocket
-  instead of `wss://`.
+1.  Maybe you are on an `https://` domain trying to use an `ws://` websocket
+    instead of `wss://`.
 
-  2. You gave an invalid URL or something crazy.
+2.  You gave an invalid URL or something crazy.
 
 -}
 type BadOpen
-  = BadSecurity
-  | BadArgs
+    = BadSecurity
+    | BadArgs
 
 
 {-| Close a `WebSocket`. If the connection is already closed, it does nothing.
 -}
 close : WebSocket -> Task x ()
 close socket =
-  Task.map
-    (always ())
-    (closeWith 1000 "" socket)
+    Task.map
+        (always ())
+        (closeWith 1000 "" socket)
 
 
 {-| Closes the `WebSocket`. If the connection is already closed, it does nothing.
 
 In addition to providing the `WebSocket` you want to close, you must provide:
 
-  1. A status code explaining why the connection is being closed. The default
-  value is 1000, indicating indicates a normal "transaction complete" closure.
-  There are a ton of different status codes though. See them all
-  [here](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent).
+1.  A status code explaining why the connection is being closed. The default
+    value is 1000, indicating indicates a normal "transaction complete" closure.
+    There are a ton of different status codes though. See them all
+    [here](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent).
 
-  2. A human-readable string explaining why the connection is closing. This
-  string must be no longer than 123 bytes of UTF-8 text (not characters).
+2.  A human-readable string explaining why the connection is closing. This
+    string must be no longer than 123 bytes of UTF-8 text (not characters).
 
 -}
 closeWith : Int -> String -> WebSocket -> Task x (Maybe BadClose)
 closeWith =
-  Native.WebSocket.close
+    Native.WebSocket.close
 
 
 {-| It is possible to provide invalid codes or reasons for closing a
@@ -125,7 +140,7 @@ task.
 -}
 send : WebSocket -> String -> Task x (Maybe BadSend)
 send =
-  Native.WebSocket.send
+    Native.WebSocket.send
 
 
 {-| There are a few ways a send can go wrong. The send function will ultimately
@@ -143,5 +158,4 @@ pile up on the queue endlessly.
 -}
 bytesQueued : WebSocket -> Task x Int
 bytesQueued =
-  Native.WebSocket.bytesQueued
-
+    Native.WebSocket.bytesQueued
